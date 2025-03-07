49d73975ef5fd9095cb845d03e885d6fe696144c
Doug Andrews 13 <dougandrews13@doug-andrews-computer-3.local>
2025-03-07 09:41:58 +0100

    Remove extra next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
+    // Add SVG support
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
     return config;
   },
 };
